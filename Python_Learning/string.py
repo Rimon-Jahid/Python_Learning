152929e6b1026cb57f085c472bc44c746e29d440
Rimon-Jahid <rimongith@gmail.com>
2026-05-05 23:00:45 +0600

    All type of string

---
+++ Python_Learning/string.py
@@ -0,0 +1,19 @@
+Pera = "I love my country"
+print(Pera)
+print(Pera[0:6]) # this print from I to love
+print(Pera[:6]) # this print from I to love
+print(Pera[-1]) # y
+print(Pera[7:]) # this print from my
+print(Pera[0:-1]) # this print from I to r
+print(Pera[:]) # this print all string
+
+pera2 = "I love Bangladeshi's food" # string with (')
+pera3 = 'I love "bangladesh"' # string with ("")
+pera4 = '''My name is Md Jahidul Islam Rimon
+My hobby playing game
+thank you
+Rimon's
+ '''
+print(pera2)
+print(pera3)
+print(pera4)
