568f9beff9c8517bcbab329974e2ac4b65da3fff
Rimon-Jahid <rimongith@gmail.com>
2026-05-05 21:01:42 +0600

    Update variable with fString

--- Python_Learning/Variable.py
+++ Python_Learning/Variable.py
@@ -2,4 +2,4 @@ name = 'Rimon'
 Age = 23
 new_patient = True
 #Python dont requare any variable type like c java
-print("His name is ",name,"his age = ",Age,"Is he new patient ",new_patient)
+print(f"His name is {name} his age = {Age} Is he new patient {new_patient}")
