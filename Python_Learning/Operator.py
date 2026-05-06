8e863b0166e1ef78255ae4f08daf877aa479158b
Rimon-Jahid <rimongith@gmail.com>
2026-05-06 16:04:48 +0600

    Update operator

--- Python_Learning/Operator.py
+++ Python_Learning/Operator.py
@@ -1,3 +1,9 @@
 num = 10
 num += 20
+num2 = 10 ** 2 # this 10 to the power 2
+num3 = 10 // 3 # this is whole number division
+num4 = 10 / 3 # this is floating point division
 print(num)
+print(num2)
+print(num3)
+print(num4)
