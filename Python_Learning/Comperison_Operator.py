bf7b95eedf2155970bf9a8c4249fa54fa01651e3
Rimon-Jahid <rimongith@gmail.com>
2026-05-06 19:27:02 +0600

    Updated

--- Python_Learning/Comperison_Operator.py
+++ Python_Learning/Comperison_Operator.py
@@ -1,6 +1,12 @@
 state = False
 creadit = True
+temperature = 30
 if creadit and not state :
     print("eligible for loan")
 else :
     print("Not for eligible for loan")
+
+if temperature != 30 :
+    print("Its not 30")
+else :
+    print("Its 30")
