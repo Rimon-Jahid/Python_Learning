e6fe8f192ea89b6167ec20df3e338a7ece8713d0
Rimon-Jahid <rimongith@gmail.com>
2026-05-07 23:24:57 +0600

    this is tuple

---
+++ Python_Learning/Tuple.py
@@ -0,0 +1,7 @@
+list = (1,2,3)
+
+# this is tuple . In tuple list is covert by first bracket not in third
+#also we cant change the list this is permanent list.
+# list.append(5) this is error
+
+print(list)
