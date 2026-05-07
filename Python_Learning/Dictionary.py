69dce28f896aee54844d625ae404fc13fea7ebd3
Rimon-Jahid <rimongith@gmail.com>
2026-05-07 23:51:05 +0600

    Dictionary mathod

---
+++ Python_Learning/Dictionary.py
@@ -0,0 +1,12 @@
+Customer = {
+    "name": "John Doe",
+    "age": 30,
+    "city": "New York",
+    "Worked": ["Software Engineer", "Data Analyst", "Project Manager", "Consultant"]
+}
+Customer["birthday"] = "01/01/1990"
+print(Customer["name"])
+print(Customer["age"])
+print(Customer["city"])
+print(Customer["Worked"])
+print(Customer["birthday"])
