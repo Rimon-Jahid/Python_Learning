69b59d7cfb266dc6b5d67fb6c6b71490bbf869dd
Rimon-Jahid <rimongith@gmail.com>
2026-05-08 19:27:15 +0600

    simple example for dictionary

--- Python_Learning/Dictionary.py
+++ Python_Learning/Dictionary.py
@@ -1,12 +1,17 @@
-Customer = {
-    "name": "John Doe",
-    "age": 30,
-    "city": "New York",
-    "Worked": ["Software Engineer", "Data Analyst", "Project Manager", "Consultant"]
+phone = input("Phone: ")
+phone_Num = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "0": "Zero"
 }
-Customer["birthday"] = "01/01/1990"
-print(Customer["name"])
-print(Customer["age"])
-print(Customer["city"])
-print(Customer["Worked"])
-print(Customer["birthday"])
+output = ""
+for ch in phone:
+    output += phone_Num.get(ch, "!") + " "
+print(output)
